3efa7a3960a356249d58a1a388167c6824621300
Seedgou <i@zczc.cz>
2022-12-28 07:15:09 +0000

    basic app layout

---
+++ src/components/header/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div>Header</div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  max-height: 100vh;
+}
+
+@media (max-width: 1023px) {
+  .nav {
+    margin-bottom: 25px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </main>
 </template>
+
+<script setup lang="ts"></script>
