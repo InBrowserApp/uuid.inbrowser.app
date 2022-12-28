0cbc99fdfcdd5c4b4212c8ba9abc6423f6e195dd
Seedgou <i@zczc.cz>
2022-12-28 07:22:03 +0000

    add uuidv4 view and description

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import UUIDV4View from "../views/UUIDV4View.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/v4",
+      name: "uuidv4",
+      component: UUIDV4View,
     },
   ],
 });
---
+++ src/views/UUIDV4View.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <UUIDV4Description />
+  </main>
+</template>
+
+<script setup lang="ts">
+import UUIDV4Description from "@/components/uuid/description/UUIDV4Description.vue";
+</script>
