87238fc5aad6888b613efa771ec2a03a06c375e8
Seedgou <i@zczc.cz>
2022-12-28 08:09:21 +0000

    add buttons

--- src/components/uuid/generator/UUIDV4Generator.vue
+++ src/components/uuid/generator/UUIDV4Generator.vue
@@ -1,11 +1,32 @@
 <template>
-  <UUIDDisplay :uuid="uuid" />
+  <div>
+    <UUIDDisplay :uuid="uuid" />
+  </div>
+  <div>
+    <n-space>
+      <RegenerateButton @click="refresh" />
+      <CopyToClipboardButton @click="copyToClipboard" />
+    </n-space>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { v4 as uuidv4 } from "uuid";
 import UUIDDisplay from "@/components/uuid/UUIDDisplay.vue";
+import { NSpace, useMessage } from "naive-ui";
+import RegenerateButton from "@/components/controls/RegenerateButton.vue";
+import CopyToClipboardButton from "@/components/controls/CopyToClipboardButton.vue";
 
+const message = useMessage();
 const uuid = ref(uuidv4());
+
+const refresh = () => {
+  uuid.value = uuidv4();
+};
+
+const copyToClipboard = () => {
+  navigator.clipboard.writeText(uuid.value);
+  message.success("Copied to clipboard");
+};
 </script>
