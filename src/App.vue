9c132b5d8b0a8fc31ecd367b2cb45460d9fdd8ac
Seedgou <i@zczc.cz>
2022-12-29 06:39:55 +0000

    update layout

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,7 @@ body {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
+  width: 100%;
 }
 
 @media (min-width: 1024px) {
--- src/views/UUIDDecodeView.vue
+++ src/views/UUIDDecodeView.vue
@@ -4,15 +4,13 @@
     <UUIDInput v-model:uuid="uuid" />
     <n-h2 prefix="bar" align-text>Result</n-h2>
     <DecodeResult :uuid="uuid" />
-    <n-h2 prefix="bar" align-text>What is a version 1 UUID?</n-h2>
-    <UUIDV1Description />
+    <div style="width: 100%" />
   </main>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { NH2 } from "naive-ui";
-import UUIDV1Description from "@/components/uuid/description/UUIDV1Description.vue";
 import DecodeResult from "@/components/uuid/decode/DecodeResult.vue";
 import UUIDInput from "@/components/misc/UUIDInput.vue";
 import { v4 as uuidv4 } from "uuid";
