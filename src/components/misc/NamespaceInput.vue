3a699e5346fdc37d540fb301a63e78b9dd40a0d6
Seedgou <i@zczc.cz>
2022-12-29 11:20:36 +0000

    add predefined namespace

--- src/components/misc/NamespaceInput.vue
+++ src/components/misc/NamespaceInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-form-item label="Namespace">
+  <n-form-item label="Namespace" :show-feedback="false">
     <n-input
       v-model:value="namespaceRef"
       :placeholder="placeholder"
@@ -13,12 +13,38 @@
       /></template>
     </n-input>
   </n-form-item>
+  <n-space style="margin-top: 0.3em; margin-bottom: 0.7em">
+    <n-tag
+      :bordered="false"
+      class="predefined"
+      @click="namespaceRef = '6ba7b810-9dad-11d1-80b4-00c04fd430c8'"
+      >ns:DNS</n-tag
+    >
+    <n-tag
+      :bordered="false"
+      class="predefined"
+      @click="namespaceRef = '6ba7b811-9dad-11d1-80b4-00c04fd430c8'"
+      >ns:URL</n-tag
+    >
+    <n-tag
+      :bordered="false"
+      class="predefined"
+      @click="namespaceRef = '6ba7b812-9dad-11d1-80b4-00c04fd430c8'"
+      >ns:OID</n-tag
+    >
+    <n-tag
+      :bordered="false"
+      class="predefined"
+      @click="namespaceRef = '6ba7b814-9dad-11d1-80b4-00c04fd430c8'"
+      >ns:X.500 DN</n-tag
+    >
+  </n-space>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, watch } from "vue";
 
-import { NInput, NIcon, NFormItem } from "naive-ui";
+import { NInput, NIcon, NFormItem, NTag, NSpace } from "naive-ui";
 import RefreshOutline from "@vicons/ionicons5/RefreshOutline";
 import { v4 as uuidv4, validate } from "uuid";
 
@@ -47,3 +73,9 @@ watch(
   }
 );
 </script>
+
+<style scoped>
+.predefined {
+  cursor: pointer;
+}
+</style>
