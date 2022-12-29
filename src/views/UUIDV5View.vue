d81d6e2b54a5d31a82564531adc901c336cf050a
Seedgou <i@zczc.cz>
2022-12-29 01:30:53 +0000

    add uuid v5

--- src/views/UUIDV5View.vue
+++ src/views/UUIDV5View.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <n-h2 prefix="bar" align-text>Version 3 UUID</n-h2>
-    <UUIDV3Generator :config="config" />
+    <UUIDV5Generator :config="config" />
     <n-h2 prefix="bar" align-text>Config</n-h2>
     <UUIDV35Config v-model:config="config" />
     <n-h2 prefix="bar" align-text>What is a version 3 UUID?</n-h2>
@@ -14,7 +14,7 @@ import { ref } from "vue";
 import { NH2 } from "naive-ui";
 import type { UUIDV35Config as UUIDV35ConfigType } from "@/utils/uuid/config/UUIDV35Config";
 import UUIDV1Description from "@/components/uuid/description/UUIDV1Description.vue";
-import UUIDV3Generator from "@/components/uuid/generator/UUIDV3Generator.vue";
+import UUIDV5Generator from "@/components/uuid/generator/UUIDV5Generator.vue";
 import UUIDV35Config from "@/components/uuid/config/UUIDV35Config.vue";
 import { v4 as uuidv4 } from "uuid";
 
