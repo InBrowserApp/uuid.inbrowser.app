ad6bc7525a8d18d4514271a9e1f47414e56ef781
Seedgou <i@zczc.cz>
2022-12-29 08:45:29 +0000

    add home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,42 @@
 <template>
   <main>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <n-h2 prefix="bar" align-text>Get a Random UUID</n-h2>
+    <UUIDV4Generator />
+    <n-h2 prefix="bar" align-text>Other Tools</n-h2>
+    UUID Generator:
+    <router-link to="/v1" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 1</n-a>
+    </router-link>
+    <n-divider vertical />
+    <router-link to="/v3" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 3</n-a>
+    </router-link>
+    <n-divider vertical />
+    <router-link to="/v4" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 4</n-a>
+    </router-link>
+    <n-divider vertical />
+    <router-link to="/v5" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 5</n-a>
+    </router-link>
+    <br />
+    Bulk UUID Generator:
+    <router-link to="/bulk/v1" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 1</n-a>
+    </router-link>
+    <n-divider vertical />
+    <router-link to="/bulk/v4" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Version 4</n-a>
+    </router-link>
+    <br />
+    Utilities:
+    <router-link to="/decode" #="{ navigate, href }" custom>
+      <n-a :href="href" @click="navigate">Decode UUID</n-a>
+    </router-link>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NH2, NDivider, NA } from "naive-ui";
+import UUIDV4Generator from "@/components/uuid/generator/UUIDV4Generator.vue";
+</script>
