8cdbc6aa495ab8df992b51003d0d0dd508dd1650
Seedgou <i@zczc.cz>
2022-12-30 16:23:15 +0000

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+      ],
+      manifest: {
+        name: "UUID InBrowser.App",
+        short_name: "UUID",
+        description:
+          "UUID InBrowser.App is a tool to generate and decode UUIDs. Fully runs in your browser, no data is sent to the server. Fast, secure, and offline capable.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
