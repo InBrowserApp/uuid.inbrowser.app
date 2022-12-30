a0487d86554541b831f256bcbbe7d72898b2271d
Seedgou <i@zczc.cz>
2022-12-30 09:03:22 +0000

    add nav github and center in mobile

--- src/components/header/nav/HeaderNav.vue
+++ src/components/header/nav/HeaderNav.vue
@@ -1,7 +1,25 @@
 <template>
-  <NavTools />
+  <nav class="header-nav">
+    <NavTools />
+    <n-divider vertical />
+    <NavGitHub />
+  </nav>
 </template>
 
 <script lang="ts" setup>
+import { NDivider } from "naive-ui";
+
 import NavTools from "./NavTools.vue";
+import NavGitHub from "./NavGitHub.vue";
 </script>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .header-nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+}
+</style>
